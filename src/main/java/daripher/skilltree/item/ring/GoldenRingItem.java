164187c932a2c96b07b08465a94af655f7d86406
Daripher <saelver.serbek@gmail.com>
2023-07-27 06:02:13 +0500

    Ring items

---
+++ src/main/java/daripher/skilltree/item/ring/GoldenRingItem.java
@@ -0,0 +1,27 @@
+package daripher.skilltree.item.ring;
+
+import java.util.UUID;
+
+import com.google.common.collect.LinkedHashMultimap;
+import com.google.common.collect.Multimap;
+
+import daripher.skilltree.init.PSTAttributes;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier.Operation;
+import net.minecraft.world.item.ItemStack;
+import top.theillusivec4.curios.api.SlotContext;
+
+public class GoldenRingItem extends RingItem {
+	@Override
+	public Multimap<Attribute, AttributeModifier> getAttributeModifiers(SlotContext slotContext, UUID uuid, ItemStack stack) {
+		Multimap<Attribute, AttributeModifier> modifiers = LinkedHashMultimap.create();
+		modifiers.put(PSTAttributes.EVASION.get(), new AttributeModifier(uuid, "Ring Bonus", 0.01, Operation.MULTIPLY_BASE));
+		return modifiers;
+	}
+
+	@Override
+	public boolean makesPiglinsNeutral(SlotContext slotContext, ItemStack stack) {
+		return true;
+	}
+}
