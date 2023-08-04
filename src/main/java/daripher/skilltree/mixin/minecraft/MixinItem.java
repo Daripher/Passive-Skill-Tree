586f2243c3541af8b95165e33d7ff0625cfdc4ab
Daripher <saelver.serbek@gmail.com>
2023-08-04 16:03:17 +0500

    Improved compatibility for crafted food bonuses

--- src/main/java/daripher/skilltree/mixin/minecraft/MixinItem.java
+++ src/main/java/daripher/skilltree/mixin/minecraft/MixinItem.java
@@ -1,16 +1,37 @@
 package daripher.skilltree.mixin.minecraft;
 
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import daripher.skilltree.util.FoodHelper;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.extensions.IForgeItem;
 
 @Mixin(Item.class)
-public class MixinItem {
+public abstract class MixinItem implements IForgeItem {
 	@Redirect(method = "getBarWidth", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/Item;getMaxDamage(Lnet/minecraft/world/item/ItemStack;)I"))
-	private int applyBonusDurability(Item item, ItemStack itemStack) {
-		return itemStack.getMaxDamage();
+	private int applyBonusDurability(Item item, ItemStack stack) {
+		return stack.getMaxDamage();
+	}
+
+	@Override
+	public FoodProperties getFoodProperties(ItemStack stack, @Nullable LivingEntity entity) {
+		FoodProperties properties = IForgeItem.super.getFoodProperties(stack, entity);
+		if (properties == null) return properties;
+		if (!FoodHelper.hasRestorationBonus(stack)) return properties;
+		float restorationBonus = 1 + FoodHelper.getRestorationBonus(stack);
+		FoodProperties.Builder newProperties = new FoodProperties.Builder();
+		if (properties.canAlwaysEat()) newProperties.alwaysEat();
+		if (properties.isFastFood()) newProperties.fast();
+		if (properties.isMeat()) newProperties.meat();
+		properties.getEffects().forEach(pair -> newProperties.effect(pair::getFirst, pair.getSecond()));
+		newProperties.nutrition((int) (properties.getNutrition() * restorationBonus));
+		newProperties.saturationMod(properties.getSaturationModifier() / restorationBonus);
+		return newProperties.build();
 	}
 }
