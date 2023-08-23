c616a37e4226e591480dadc44ae44c0d500f2f05
Daripher <saelver.serbek@gmail.com>
2023-08-23 12:11:05 +0500

    Editor command now suggests existing tree ids

--- src/main/java/daripher/skilltree/client/command/PSTClientCommands.java
+++ src/main/java/daripher/skilltree/client/command/PSTClientCommands.java
@@ -4,12 +4,15 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
 
 import daripher.skilltree.SkillTreeMod;
+import daripher.skilltree.client.SkillTreeClientData;
 import daripher.skilltree.client.screen.editor.SkillTreeEditorScreen;
 import net.minecraft.client.Minecraft;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.commands.SharedSuggestionProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterClientCommandsEvent;
@@ -18,11 +21,16 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
 @EventBusSubscriber(modid = SkillTreeMod.MOD_ID, value = Dist.CLIENT)
 public class PSTClientCommands {
+	public static final SuggestionProvider<CommandSourceStack> SUGGEST_SKILL_TREE_ID_PROVIDER = (ctx, builder) -> {
+		return SharedSuggestionProvider.suggest(SkillTreeClientData.getTreeIds().stream().map(ResourceLocation::toString), builder);
+	};
+
 	@SubscribeEvent
 	public static void registerCommands(RegisterClientCommandsEvent event) {
 		LiteralArgumentBuilder<CommandSourceStack> editorCommand = Commands.literal("skilltree")
 				.then(Commands.literal("editor")
-						.then(Commands.argument("treeId", StringArgumentType.string())
+						.then(Commands.argument("treeId", StringArgumentType.greedyString())
+								.suggests(SUGGEST_SKILL_TREE_ID_PROVIDER)
 								.executes(PSTClientCommands::displaySkillTreeEditor)));
 		event.getDispatcher().register(editorCommand);
 	}
