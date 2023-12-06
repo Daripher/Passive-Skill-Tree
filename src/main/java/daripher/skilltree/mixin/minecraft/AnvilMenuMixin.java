cb3fc91488f5702e941b2510b6ad5d0b1856ba61
Daripher <saelver.serbek@gmail.com>
2023-12-07 00:19:00 +0500

    Fixed #66

--- src/main/java/daripher/skilltree/mixin/minecraft/AnvilMenuMixin.java
+++ src/main/java/daripher/skilltree/mixin/minecraft/AnvilMenuMixin.java
@@ -1,13 +1,21 @@
 package daripher.skilltree.mixin.minecraft;
 
 import net.minecraft.world.inventory.AnvilMenu;
+import net.minecraft.world.inventory.ItemCombinerMenu;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantment;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(AnvilMenu.class)
-public class AnvilMenuMixin {
+public abstract class AnvilMenuMixin extends ItemCombinerMenu {
+  @SuppressWarnings("DataFlowIssue")
+  public AnvilMenuMixin() {
+    super(null, 0, null, null);
+  }
+
   @Redirect(
       method = "createResult",
       at =
@@ -16,6 +24,11 @@ public class AnvilMenuMixin {
               target = "Lnet/minecraft/world/item/enchantment/Enchantment;getMaxLevel()I"),
       require = 0)
   private int uncapEnchantmentLevel(Enchantment enchantment) {
-    return Integer.MAX_VALUE;
+    ItemStack base = inputSlots.getItem(0);
+    ItemStack addition = inputSlots.getItem(1);
+    if (base.getAllEnchantments().isEmpty() && addition.getItem() == Items.ENCHANTED_BOOK) {
+      return Integer.MAX_VALUE;
+    }
+    return enchantment.getMaxLevel();
   }
 }
