23d21da0bf55f67d8a6aeafff40582556d903c27
Daripher <saelver.serbek@gmail.com>
2023-12-10 11:42:43 +0500

    Fixed broken bonuses of tourmaline gem

--- src/main/java/daripher/skilltree/item/gem/TourmalineItem.java
+++ src/main/java/daripher/skilltree/item/gem/TourmalineItem.java
@@ -1,12 +1,20 @@
 package daripher.skilltree.item.gem;
 
+import daripher.skilltree.skill.bonus.condition.item.FoodCondition;
 import daripher.skilltree.skill.bonus.item.FoodHealingBonus;
 import daripher.skilltree.skill.bonus.item.FoodSaturationBonus;
+import daripher.skilltree.skill.bonus.item.ItemSkillBonus;
+import daripher.skilltree.skill.bonus.player.CraftedItemBonus;
 
 public class TourmalineItem extends SimpleGemItem {
   public TourmalineItem() {
     super();
-    setBonuses(new FoodHealingBonus(0.25f), "necklace");
-    setBonuses(new FoodSaturationBonus(0.1f), "ring");
+    setBonuses(
+        new ItemSkillBonus(new CraftedItemBonus(new FoodCondition(), new FoodHealingBonus(0.25f))),
+        "necklace");
+    setBonuses(
+        new ItemSkillBonus(
+            new CraftedItemBonus(new FoodCondition(), new FoodSaturationBonus(0.1f))),
+        "ring");
   }
 }
