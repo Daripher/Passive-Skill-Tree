cfd028cbbc7755744ac6ac97cac096e590beaba3
Daripher <saelver.serbek@gmail.com>
2024-01-15 00:42:22 +0500

    Copied skill bonuses now have different UUIDs

--- src/main/java/daripher/skilltree/client/screen/SkillTreeEditorScreen.java
+++ src/main/java/daripher/skilltree/client/screen/SkillTreeEditorScreen.java
@@ -330,7 +330,7 @@ public class SkillTreeEditorScreen extends Screen {
               this,
               selectedBonus,
               b -> {
-                selectedSkills().forEach(s -> s.getBonuses().set(selectedBonus, b));
+                selectedSkills().forEach(s -> s.getBonuses().set(selectedBonus, b.copy()));
                 saveSelectedSkills();
               });
     }
