c44bb12058ff61bc844b0b4bd763f6c2c9ab0893
Daripher <saelver.serbek@gmail.com>
2025-08-26 03:57:34 +0500

    Potions are no longer considered equipment while on offhand

--- src/main/java/daripher/skilltree/entity/player/PlayerHelper.java
+++ src/main/java/daripher/skilltree/entity/player/PlayerHelper.java
@@ -2,68 +2,46 @@ package daripher.skilltree.entity.player;
 
 import com.google.common.collect.Streams;
 import daripher.skilltree.skill.bonus.condition.item.EquipmentCondition;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
-
-import daripher.skilltree.skill.bonus.condition.item.PotionCondition;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.stats.Stats;
-import net.minecraft.util.Mth;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.common.ToolActions;
-import net.minecraftforge.event.ForgeEventFactory;
 import org.jetbrains.annotations.NotNull;
 import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
 
 public class PlayerHelper {
-  public static void hurtShield(Player player, final ItemStack shield, float amount) {
-    if (!shield.canPerformAction(ToolActions.SHIELD_BLOCK)) return;
-    if (!player.level().isClientSide) {
-      player.awardStat(Stats.ITEM_USED.get(shield.getItem()));
-    }
-    if (amount < 3) return;
-    amount = 1 + Mth.floor(amount);
-    shield.hurtAndBreak(
-        (int) amount,
-        player,
-        p -> {
-          p.broadcastBreakEvent(InteractionHand.OFF_HAND);
-          ForgeEventFactory.onPlayerDestroyItem(player, shield, InteractionHand.OFF_HAND);
-        });
-    if (shield.isEmpty()) {
-      player.setItemSlot(EquipmentSlot.OFFHAND, ItemStack.EMPTY);
-      player.playSound(
-          SoundEvents.SHIELD_BREAK, 0.8F, 0.8F + player.level().random.nextFloat() * 0.4F);
-    }
-  }
-
   public static Stream<ItemStack> getAllEquipment(LivingEntity living) {
-    return Streams.concat(getEquipment(living), getCurios(living));
+    return Streams.concat(getVanillaEquipment(living), getCurios(living));
   }
 
   public static Stream<ItemStack> getItemsInHands(LivingEntity living) {
     return Stream.of(living.getMainHandItem(), living.getOffhandItem());
   }
 
-  public static Stream<ItemStack> getEquipment(LivingEntity living) {
-    return Arrays.stream(EquipmentSlot.values()).map(slot -> getEquipment(living, slot));
+  public static Stream<ItemStack> getVanillaEquipment(LivingEntity living) {
+    return Arrays.stream(EquipmentSlot.values()).map(slot -> getEquipmentInSlot(living, slot));
+  }
+
+  public static Stream<ItemStack> getArmor(LivingEntity living) {
+    return Arrays.stream(EquipmentSlot.values())
+        .filter(EquipmentSlot::isArmor)
+        .map(slot -> getEquipmentInSlot(living, slot));
   }
 
   @NotNull
-  private static ItemStack getEquipment(LivingEntity living, EquipmentSlot slot) {
+  private static ItemStack getEquipmentInSlot(LivingEntity living, EquipmentSlot slot) {
     ItemStack stack = living.getItemBySlot(slot);
     if (slot == EquipmentSlot.MAINHAND
         && !EquipmentCondition.isWeapon(stack)
         && !EquipmentCondition.isTool(stack)
         && !EquipmentCondition.isPotion(stack)) {
+      return ItemStack.EMPTY;
+    }
+    if (slot == EquipmentSlot.OFFHAND && EquipmentCondition.isPotion(stack)) {
       return ItemStack.EMPTY;
     }
     return stack;
