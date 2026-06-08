c42bbdd7c06cad18371ae95426cae88909072965
Daripher <saelver.serbek@gmail.com>
2026-06-08 23:24:14 +0500

    Fixed texture data gathering taking too much time to load, especially with big modpacks

--- src/main/java/daripher/skilltree/client/data/SkillTexturesData.java
+++ src/main/java/daripher/skilltree/client/data/SkillTexturesData.java
@@ -2,6 +2,7 @@ package daripher.skilltree.client.data;
 
 import daripher.skilltree.SkillTreeMod;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
 import net.minecraftforge.api.distmarker.Dist;
@@ -25,16 +26,36 @@ public class SkillTexturesData implements ResourceManagerReloadListener {
     @Override
     public void onResourceManagerReload(@NotNull ResourceManager resourceManager) {
         FOLDER_TO_TEXTURES.clear();
-        Collection<String> namespaces = resourceManager.getNamespaces();
-        for (String ignored : namespaces) {
-            List<ResourceLocation> textureLocations = resourceManager.listResources("textures", SkillTexturesData::isTexturePath).keySet()
-                    .stream().toList();
-            for (ResourceLocation textureLocation : textureLocations) {
-                String folder = getTextureFolder(textureLocation);
-                FOLDER_TO_TEXTURES.computeIfAbsent(folder, f -> new HashSet<>());
-                FOLDER_TO_TEXTURES.get(folder).add(textureLocation);
+        Map<ResourceLocation, Resource> textures = resourceManager.listResources("textures", SkillTexturesData::isTexturePath);
+        List<ResourceLocation> textureLocations = textures.keySet().stream().toList();
+        for (ResourceLocation textureLocation : textureLocations) {
+            String folder = getTextureFolder(textureLocation);
+            if (folder.isEmpty()) {
+                continue;
             }
+            FOLDER_TO_TEXTURES.computeIfAbsent(folder, f -> new HashSet<>()).add(textureLocation);
         }
+    }
+
+    @NotNull
+    public static String getTextureFolder(ResourceLocation textureLocation) {
+        String path = textureLocation.getPath();
+        int lastSlash = path.lastIndexOf('/');
+        if (lastSlash <= 0) {
+            return "";
+        }
+        return path.substring(0, lastSlash);    }
+
+    private static boolean isTexturePath(ResourceLocation location) {
+        return location.getPath().endsWith(".png");
+    }
+
+    public static Set<ResourceLocation> getTexturesInFolder(String folder) {
+        return FOLDER_TO_TEXTURES.getOrDefault(folder, Set.of());
+    }
+
+    public static boolean isTextureFolder(String string) {
+        return FOLDER_TO_TEXTURES.containsKey(string);
     }
 
     @Nullable
@@ -42,27 +63,5 @@ public class SkillTexturesData implements ResourceManagerReloadListener {
         Set<String> folders = FOLDER_TO_TEXTURES.keySet();
         Optional<String> autocomplete = folders.stream().filter(s -> s.startsWith(string)).findAny().map(s -> s.replaceFirst(string, ""));
         return autocomplete.orElse(null);
-    }
-
-    @NotNull
-    public static String getTextureFolder(ResourceLocation textureLocation) {
-        String location = textureLocation.toString();
-        location = location.substring(0, location.lastIndexOf('/'));
-        if (!location.contains("/")) {
-            return "";
-        }
-        return location.substring(location.indexOf("/") + 1);
-    }
-
-    public static Set<ResourceLocation> getTexturesInFolder(String folder) {
-        return FOLDER_TO_TEXTURES.getOrDefault(folder, Set.of());
-    }
-
-    private static boolean isTexturePath(ResourceLocation location) {
-        return location.getPath().endsWith(".png");
-    }
-
-    public static boolean isTextureFolder(String string) {
-        return FOLDER_TO_TEXTURES.containsKey(string);
     }
 }
